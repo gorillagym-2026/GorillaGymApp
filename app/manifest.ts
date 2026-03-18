9560c85edb18d18edd5c6dbf12263a801f7cf7bc
Fabricio Barreto <fabrib40@gmail.com>
2026-03-18 20:25:10 -0300

    added

--- app/manifest.ts
+++ app/manifest.ts
@@ -16,25 +16,13 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/android/android-launchericon-192-192.png",
         sizes: "192x192",
         type: "image/png",
-        purpose: "any",
-      },
-      {
-        src: "/android/android-launchericon-192-192.png",
-        sizes: "192x192",
-        type: "image/png",
-        purpose: "maskable",
+        purpose: "any", // ← solo "any", sin maskable
       },
       {
         src: "/android/android-launchericon-512-512.png",
         sizes: "512x512",
         type: "image/png",
-        purpose: "any",
-      },
-      {
-        src: "/android/android-launchericon-512-512.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "maskable",
+        purpose: "any", // ← solo "any", sin maskable
       },
     ],
   };
